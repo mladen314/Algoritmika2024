8add8c2cdb00ceb97f9fea0e3c23d60ceb654aa2
vuklekic1 <vuk34039@gmail.com>
2024-12-07 18:24:37 +0100

    a

--- VukL/script.js
+++ VukL/script.js
@@ -1,10 +1,11 @@
-function convertToMillimeters(centimeters) {
-    return centimeters * 10;  
-  }
-  
-  let centimeters = 5;  
-  let millimeters = convertToMillimeters(centimeters);
-  
-  console.log(`${centimeters} centimeters is equal to ${millimeters} millimeters.`);
 
+    const cm = document.getElementById("centimeters").value;
+    const resultDiv = document.getElementById("result");
+
+    if (cm === "" || cm <= 0) {
+      resultDiv.innerHTML = "<span>Please enter a valid number greater than 0.</span>";
+    } else {
+      const mm = cm * 10;
+      resultDiv.innerHTML = `<span>${cm} cm</span> is equal to <span>${mm} mm</span>`;
+    }
   
