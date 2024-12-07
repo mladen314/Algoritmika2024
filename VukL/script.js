b927c50a8cbb44c0cf4c9045e884d24c3b67448f
vuklekic1 <vuk34039@gmail.com>
2024-12-07 18:12:16 +0100

    js

---
+++ VukL/script.js
@@ -0,0 +1,8 @@
+function convertToMillimeters(centimeters) {
+    return centimeters * 10;  
+  }
+  
+  let centimeters = 5;  
+  let millimeters = convertToMillimeters(centimeters);
+  
+  console.log(`${centimeters} centimeters is equal to ${millimeters} millimeters.`);
