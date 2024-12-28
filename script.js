373704027a6700adff4553d8ec792c19f1a062ba
mladen314 <144899141+mladen314@users.noreply.github.com>
2024-12-28 19:14:12 +0100

    a

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("OVAJ ZADATAK RADI")
+
+let block = document.querySelector('.block')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
